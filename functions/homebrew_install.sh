01713769419a955d38b52cc037b32b206e4877d8
na9amura <4410504+na9amura@users.noreply.github.com>
2023-07-19 14:59:17 +0900

    update homebrew setup

--- functions/homebrew_install.sh
+++ functions/homebrew_install.sh
@@ -7,10 +7,12 @@ function install_homebrew_and_libraries {
     brew_command cleanup
   else
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+     (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
   fi
 
   print_info "start brew install..."
-  brew bundle
+  brew bundle --file ~/.config/brewfile/Brewfile
 
   # sudo ln -sfn $(brew --prefix)/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
 }
